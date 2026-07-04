de64bed0bc0a08b6f0756db691da395753fc4db5
Zack-River <elmutamiz@gmail.com>
2026-07-04 21:32:07 +0300

    update initial WA message

--- src/components/WhatsAppButton.jsx
+++ src/components/WhatsAppButton.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 import { FaWhatsapp } from 'react-icons/fa';
 import './WhatsAppButton.css';
 
-const WhatsAppButton = ({ phoneNumber = ""}) => {
+const WhatsAppButton = ({ phoneNumber = "966575109308", message = "هلا ، ودي استفسر عن دليل ادوات الذكاء الاصطناعي." }) => {
   // If no phone number is provided, we can fallback to a generic link or just open WhatsApp
   const whatsappUrl = phoneNumber 
-    ? `https://wa.me/${phoneNumber}`
+    ? `https://wa.me/${phoneNumber}?text=${message}`
     : `https://wa.me/`;
 
   return (
